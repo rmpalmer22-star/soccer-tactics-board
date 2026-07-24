c0e6768f0ab28521f26116ca838324f7eb236a00
rmpalmer22-star <rmpalmer22@gmail.com>
2026-07-23 21:35:17 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,15 @@
-const CACHE='soccer-tactics-v1';
-const ASSETS=['./','./index.html','./manifest.json'];
+const CACHE='soccer-tactics-v2';
+const ASSETS=['./','./index.html','./manifest.json','./js/app.js','./js/firebase.js'];
 self.addEventListener('install',e=>{
  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
  self.skipWaiting();
 });
 self.addEventListener('activate',e=>{
- e.waitUntil(self.clients.claim());
+ e.waitUntil(
+   caches.keys().then(keys => Promise.all(
+     keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+   )).then(() => self.clients.claim())
+ );
 });
 self.addEventListener('fetch',e=>{
  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
